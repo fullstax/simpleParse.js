d23f7cedb0da9cee667a09bbf5a91f5801797764
Geoff Davis <gdavis@archermalmo.com>
2017-02-23 13:21:32 -0600

    add uglification preset/script

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 'use strict';
+const webpack = require('webpack'),
+	isDIST = process.env.DIST,
+	uglifyConfig = {
+		cacheFolder: __dirname
+	};
+
+console.log(isDIST === 'true')
 module.exports = {
   entry: './source/index.js',
   output: {
   	path: __dirname+'/dist',
-  	filename: 'index.js'
+  	filename: `index.${isDIST === 'true' ? 'min.' : ''}js`
   },
   module: {
   	loaders: [
  		{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' } 	
   	]
-  }
+  },
+  plugins: [
+  	(isDIST === 'true' ? new webpack.optimize.UglifyJsPlugin() : null)
+  ]
 };
